30335209f20f18a19d1329763a8049eb5aa434e9
Georgij Tsabiev <gtsabiev@inbox.ru>
2024-06-13 23:36:24 +0300

    Добавляет стилизацию range слайдера с помощью noUiSlider

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,13 @@
-/* в этот файл добавляет скрипты*/
+const range = document.querySelector('.range');
+
+if (range) {
+  noUiSlider.create(range, {
+    start: [0, 999999],
+    connect: true,
+    step: 1,
+    range: {
+      'min': [500],
+      'max': [999999]
+    }
+  });
+}

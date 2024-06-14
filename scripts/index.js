03e86cfd2f0b44a8954f561e0e6d9c525df75f76
georgij-tsabiev <112905032+georgij-tsabiev@users.noreply.github.com>
2024-06-14 09:59:32 +0000

    Deploying to gh-pages from @ georgij-tsabiev/drink2go-grading@82a85192f6c7ea219c39a1e357aaa811d99262de 🚀

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,1 @@
-var e=document.querySelector(".range");e&&noUiSlider.create(e,{start:[0,999999],connect:!0,step:1,range:{min:[500],max:[999999]}});
+var e=document.querySelector(".range");e&&noUiSlider.create(e,{start:[0,1e3],connect:!0,step:1,range:{min:[0],max:[1e3]}});
